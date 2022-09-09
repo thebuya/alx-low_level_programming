953481ad3062d87299ec4f54d35670d86441ca49
thebuya <thebuya23@gmail.com>
2022-09-08 19:53:36 -0700

    here

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/**
- *Description
- */
+/**                                                                                                                 
+ *  * main - Entry point                                                                                               
+ *   * Description: 'the program's description'                                                                         
+ *    * Return: Always 0 (Success)                                                                                       
+ *     */  
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
