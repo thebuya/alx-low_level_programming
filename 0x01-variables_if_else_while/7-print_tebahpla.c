f0231a0b1868f1add9342a84eed39623d6e963e7
thebuya <thebuya23@gmail.com>
2022-09-11 11:17:30 +0300

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - main function
+*
+* a program that prints the alphabet in lowercase.
+* Return: end progress
+*/
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
