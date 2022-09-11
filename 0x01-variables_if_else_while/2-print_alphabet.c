5c9f065f1dae353783008d467ad6a9ab47701302
thebuya <thebuya23@gmail.com>
2022-09-11 08:47:55 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - main function
+*
+* a program that prints the alphabet in lowercase.
+* Return: end progress
+*/
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	putchar(alphabet);
+	return (0);
+}
