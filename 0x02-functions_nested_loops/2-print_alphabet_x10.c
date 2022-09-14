54fae5906e7d5d8a1a8f4b7183a467bdc8d5003a
thebuya <thebuya23@gmail.com>
2022-09-14 16:01:54 +0300

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
 * print_alphabey_x10 - Description
-* 
+*
 * Return: 0
 */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char letter, no_of_times;
 
-	for (no_of_times =0; no_of_times <= 9; no_of_times++)
+	for (no_of_times = 0; no_of_times <= 9; no_of_times++)
 	{
-		for (letter ='a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
 		_putchar('\n');
 	}
-	
 }
