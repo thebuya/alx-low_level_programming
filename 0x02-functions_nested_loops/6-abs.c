a780027c4965572313b0e5e07b22c6fb2a027a8b
thebuya <thebuya23@gmail.com>
2022-09-14 16:28:04 +0300

    s

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,18 @@
 #include <main.h>
+#include <stdio.h>
 /**
 * _putabs - function
-*
+* @c: arg
 * Return: 0 Always
 */
-int mai
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
