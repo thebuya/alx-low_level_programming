ea26d4e23786e686c9124963bdedc2cc551a5f50
thebuya <thebuya23@gmail.com>
2022-09-14 14:54:13 +0300

    x

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 #include <unistd.h>
 #include "main.h"
 /**
-* main - print alphabet in lowercase 
-*
+* main - print_alphabet in lowercase
+* * print_alphabet - Prints 10 times the alphabet, in lowercase.
 * Return: end progress
 */
 
-
-void print_alphabet(void)
+void print_alphabet(void) /* Prints 10 times the alphabet, in lowercase. */
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
