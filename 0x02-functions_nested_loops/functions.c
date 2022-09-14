801750427c83757a801d6d37375a0132ce330db0
thebuya <thebuya23@gmail.com>
2022-09-14 05:07:15 +0300

    number1

---
+++ 0x02-functions_nested_loops/functions.c
@@ -0,0 +1,31 @@
+
+/*Void -> */ 
+Functions must be declared before their usage.
+
+Function Prototypes
+-> TYPE of value returned
+-> NAME of function
+-> No.of.Arguments Taken & TYPES
+# Return will be the same type as protpyes decalreion type.
+
+
+Automatic Variables are decalred within a function.
+Stack Vs Global
+/**/
+Function needs to be decalred before referencing
+
+/* How to Call Functions */
+1. By referencing its name, and adding any arguments if any.
+1.1 And we tend to do this while assigning it to another variable that is defined inside function main
+
+/*Functions with no type*/
+1. Use Void
+2 return; Thats how it is
+
+Unless stated wotherwise;
+	- All functions will be given int.
+
+
+/*Tips: */
+	- Good rule of Thumb; Program Defensivley; Check the arguments
+	- Header files be having protptyes functions
